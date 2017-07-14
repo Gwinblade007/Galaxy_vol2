c787f492a6c05fbedfc5d35c3910269af4d26953
Gwinblade007 <niko1ascoolman@gmail.com>
2017-07-14 03:01:17 +0200

    Done

--- Galaxy_vol_2/js/app.js
+++ Galaxy_vol_2/js/app.js
@@ -32,10 +32,16 @@ function init() {
     terrainPattern = ctx.createPattern(resources.get('img/terrain.png'), 'repeat');
 
     document.getElementById('play-again').addEventListener('click', function() {
-        reset();
+        var start = document.getElementById('start').pause();
+    	reset();
     });
-
-    reset();
+    document.getElementById('game-start').addEventListener('click', function(){
+    	isGameStart = true;
+    	var start = document.getElementById('start').pause();
+    	reset();
+    });
+    //reset();
+    gameStart();
     lastTime = Date.now();
     main();
 }
@@ -45,7 +51,7 @@ resources.load([
     'img/terrain.png',
     'img/111.png',
     'img/projectile_bolt_blue_single.png',
-    'img/strip_bug_eye.png'
+    'img/player.png'
 ]);
 resources.onReady(init);
 
@@ -57,18 +63,19 @@ var player = {
     //sprite: new Sprite('img/sprites.png', [0, 0], [39, 39], 16, [0, 1])
    
     //вертикально
-    sprite: new Sprite('img/111.png', [0, 0], [39, 39], 16,[0,1],'vertical')
+    sprite: new Sprite('img/player.png', [0, 0], [99, 75])
 };
 
 var bullets = [];
 var enemies = [];
 var waves = 1;
-var enemiesInWaves ;
+var enemiesInWaves = 3;
 var explosions = [];
 
 var lastFire = Date.now();
 var gameTime = 0;
 var isGameOver;
+var isGameStart = false;
 var terrainPattern;
 
 var score = 0;
@@ -78,21 +85,21 @@ var wavesCount = document.getElementById('waves');
 var lives = 1;
 var livesInHtml = document.getElementById('lives');
 var hurt = false;
-
+var final = document.getElementById('final');
 // Скорость
 var playerSpeed = 200;
 var bulletSpeed = 500;
-var enemySpeed = 100;
+var enemySpeed = 50;
 
 // Обновление объектов
 function update(dt) {
+   if(isGameStart){
     gameTime += dt;
 
     handleInput(dt);
     updateEntities(dt);
 
-    // It gets harder over time by adding enemies using this
-    // equation: 1-.993^gameTime
+    
     if((Math.random() < 1 - Math.pow(.993, gameTime))&&(enemies.length<=enemiesInWaves-1)) {
         enemies.push({
             pos: [ Math.random() * (canvas.width - 80),
@@ -114,6 +121,13 @@ function update(dt) {
 
 
     livesInHtml.innerHTML = "Жизней "+ lives;
+
+
+    if(enemiesInWaves==0)
+    	{
+    		waves++;
+    		enemiesInWaves = 3*waves;
+    	}}
 };
 
 function handleInput(dt) {
@@ -135,8 +149,8 @@ function handleInput(dt) {
 
     if(input.isDown('SPACE') &&
        !isGameOver &&
-       Date.now() - lastFire > 700) {
-        var x = player.pos[0] + player.sprite.size[0] / 4;
+       Date.now() - lastFire > 100) {
+        var x = player.pos[0] + 4 + player.sprite.size[0] / 3;
         var y = player.pos[1] + player.sprite.size[1] / 2;
 
        // bullets.push({ pos: [x, y],
@@ -148,7 +162,7 @@ function handleInput(dt) {
        // bullets.push({ pos: [x, y],
          //              dir: 'left',
            //            sprite: new Sprite('img/sprites.png', [0, 60], [9, 5]) });
-
+           onAudio('music/1.mp3');
         lastFire = Date.now();
     }
 }
@@ -182,7 +196,7 @@ function updateEntities(dt) {
         enemies[i].sprite.update(dt);
 
         // Удаление врагов, когда они выходят за рамки экрана
-        if(enemies[i].pos[1] + enemies[i].sprite.size[1] < 0) {
+        if(enemies[i].pos[1] + enemies[i].sprite.size[1] > 540) {
             enemies.splice(i, 1);
             i--;
         }
@@ -247,7 +261,7 @@ function checkCollisions() {
                                        null,
                                        true)
                 });
-
+                onAudio('music/exp.mp3');
                 // Удаление пули, которая взорвала врага)
                 bullets.splice(j, 1);
                 break;
@@ -256,11 +270,16 @@ function checkCollisions() {
 
         if(boxCollides(pos, size, player.pos, player.sprite.size)) {
             
-            if(lives==1)
+            if(lives==1){
             gameOver();
+
+        }
         	else 
-        		if(lives>1){player.pos = [canvas.width / 2, 1000];
-        		lives--;}
+        		if(lives>1){
+        			player.pos = [canvas.width / 2, 1000];
+        		lives--;
+        		onAudio('music/hurt.ogg');
+        	}
             
         }
         
@@ -293,11 +312,11 @@ function render() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
-
+    if(isGameStart){
     if(!isGameOver) {
         renderEntity(player);
     }
-
+}
     renderEntities(bullets);
     renderEntities(enemies);
     renderEntities(explosions);
@@ -321,23 +340,41 @@ function gameOver() {
 	lives--;
     document.getElementById('game-over').style.display = 'block';
     document.getElementById('game-over-overlay').style.display = 'block';
+    var start = document.getElementById('start').play();
     isGameOver = true;
+    isGameStart = false;
+    final.innerHTML = "Ваш счет "+score;
     
 }
 
+function gameStart(){
+	document.getElementById('game-start').style.display = 'block';
+	document.getElementById('game-over-overlay').style.display = 'block';
+    
+}
 
 function reset() {
     document.getElementById('game-over').style.display = 'none';
+    document.getElementById('game-start').style.display = 'none';
+	
     document.getElementById('game-over-overlay').style.display = 'none';
     isGameOver = false;
+    isGameStart = true;
     gameTime = 0;
     score = 0;
     lives = 3;
+    waves = 1;
 
     enemies = [];
     bullets = [];
 
     player.pos = [canvas.width / 2, 1000];
 
-    enemiesInWaves = 10*waves;
+    enemiesInWaves = 3*waves;
 };
+
+function onAudio(url){
+var name = new Audio();
+name.src=url;
+name.autoplay = true;
+}
